14c5e77922e012cf511f7bfe424257aed64e44ac
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-08-24 12:01:05 -0700

    Deleted commented out code.

--- CAH/Models/CAHContext.cs
+++ CAH/Models/CAHContext.cs
@@ -7,11 +7,6 @@ namespace CAH.Models
         public DbSet<WhiteCards> WhiteCards { get; set; }
         public DbSet<BlackCards> BlackCards { get; set; }
 
-        // public DbSet<Player> Players { get; set; }
-        // public DbSet<Hand> Hands { get; set; }
-        // public DbSet<PlayerHand> PlayerHands { get; set; }
-        // public DbSet<Deck> Decks { get; set; }
-
         public CAHContext(DbContextOptions options) : base(options) { }
     }
 }
